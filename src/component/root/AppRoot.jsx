78f2a6af8f016662bd6ae5c592dc83125af8746d
Paul <paul.bodnar@metalab.co>
2018-05-25 12:28:44 -0700

    add module resolver, globalstyles, style.constant

--- src/component/root/AppRoot.jsx
+++ src/component/root/AppRoot.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import {Switch, Route } from 'react-router-dom';
+import {ThemeProvider} from 'styled-components';
 import { hot } from 'react-hot-loader';
+
+// Import components ===========================================================
+
+// Import config ===============================================================
+
+const theme = {
+  body: 'onyx',
+  heading: 'black',
+  caption: 'darkGrey',
+  footer: 'blue',
+  background: '#fff',
+};
 
 const App = () => (
   <div>
---
+++ src/config/style.constant.js
@@ -0,0 +1,52 @@
+// @flow
+
+export const colors = {
+  black: '#101131',
+  white: '#ffffff',
+  grey: '#f4f5f7',
+};
+
+export const lineHeight = {
+  base: 1.642821,
+  decay: 0.2019217,
+};
+
+export const fontSizes = {
+  xxSmall: 0.55,
+  xSmall: 0.65,
+  small: 0.8,
+  slightlySmaller: 0.9,
+  normal: 1,
+  slightlyLarger: 1.1,
+  large: 1.5,
+  xLarge: 2,
+  xxLarge: 3.5,
+};
+
+export const letterSpacings = {
+  large: '1.8px', // TODO: -emify
+};
+
+export const fontWeights = {
+  light: '300',
+  normal: '400',
+  medium: '500',
+  bold: '700',
+};
+
+export const spacings = {
+  xxSmall: 0.3,
+  xSmall: 0.75,
+  small: 1.25,
+  normal: 2.5,
+  large: 5,
+  xLarge: 8.4,
+};
+
+export const breakpoints = {
+  xLarge: 1600,
+  large: 1200,
+  medium: 992,
+  small: 768,
+  xSmall: 420,
+};
